782e6fd05f54d809cb9df871721ebeb64db495fe
Moling Guo <morning2139@gmail.com>
2016-03-22 23:53:34 -0400

    add war log date

--- js/controllers/WarLogController.js
+++ js/controllers/WarLogController.js
@@ -5,4 +5,8 @@ app.controller('WarLogController', function($http, $scope, $location, wars, conf
 	wars.getWars().success(function(data) {
 		$scope.wars = data.reverse();
 	});
+
+	$scope.formatDate = function(date) {
+		return moment(date).format("MMM D");
+	}
 });
